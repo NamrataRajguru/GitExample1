6f1b78adfd77f6d8a029d6231d072f0f6a5c0f03
Acer <Acer@LAPTOP-MKT8NHU1>
2021-04-03 21:23:29 +0530

    [Akash]:- new demo1 class added into git_example

---
+++ src/Git_Demo/sample2.java
@@ -0,0 +1,13 @@
+package Git_Demo;
+
+public class sample2 {
+public static void main(String[] args) {
+	
+System.out.println("Hiii");	
+	
+System.out.println("new class created in Git_Example1");
+	
+	
+	
+}
+}
